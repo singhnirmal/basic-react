f1557fcf55eb40c14f02c2e86acdb67f07350bd4
nirmalbuzzyaers <nirmal.singh@buzzyears.com>
2019-09-30 10:35:06 +0530

    Get Dynamic data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then( users => this.setState({users:users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
